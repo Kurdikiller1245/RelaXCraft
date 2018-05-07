a57997835926fbd88f73ac091918767dc2dbc599
Kurdikiller1245 <38956477+Kurdikiller1245@users.noreply.github.com>
2018-05-07 19:58:59 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -27,7 +27,7 @@ message.channel.send({embed: {
     description: "",
     fields: [{
         name: "General Commands",
-        value: "!help - Display this message \n!ip - show RelaXCraft ip \n!hello",
+        value: "!help - Display this message \n!ip - show RelaXCraft ip \n!hello \n!-add - add a suggestion",
       },
       {
         name: "Staff Command",
